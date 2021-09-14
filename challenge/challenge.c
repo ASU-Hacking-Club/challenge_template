e384298a46f88d70b1c7f3fe6ec99630a62470e2
mahaloz <zbasque@asu.edu>
2021-09-13 22:12:34 -0700

    updated from kanak

--- challenge/challenge.c
+++ challenge/challenge.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main()
 {
-    setbuf(stdin, NULL);
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 1);
     printf("This is a test challenge\n");
-    system("whoami");
+    char* argv[] = {"cat", "/home/ctf/flag.txt", NULL }; 
+    execve("/bin/cat", argv, 0); 
 }
